9fe05ea0b77c522f66108139f6e6fcb485ba24d7
raphael chiche <raphael.ch2004@gmail.com>
2023-12-27 23:51:14 +0100

    🎨 Add QR code download button

--- qrcode-generator/javascript.js
+++ qrcode-generator/javascript.js
@@ -1,13 +1,14 @@
 let boxImage = document.getElementById("boxImage");
 let qrImg = document.getElementById("qrCode");
 let qrTxt = document.getElementById("qrTxt");
-
+let qrBtn = document.getElementById("telecharger");
 function generateQR() {
   if (qrTxt.value.length > 0) {
     qrImg.src =
       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
       qrTxt.value;
     boxImage.classList.add("show-img");
+    qrBtn.classList.add("show-btn");
   } else { 
     qrTxt.style.animation = "shake 0.5s";
     qrTxt.addEventListener("animationend", () => {
